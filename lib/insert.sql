daff8da65bf7c5e6de3967fdf1c96abfcc90784d
Jenny Chu <jchudesign0930@gmail.com>
2019-04-21 00:37:34 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ('Mr.Chocolate', 5, 'female', 'green', 'easy', 'alive');
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ('Rowdy', 6, 'female', 'green', 'easy', 'dead');
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ('Tabitha', 5, 'female', 'grey', 'easy', 'alive');
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ('Sergeant Brown', 5, 'male', 'black', 'easy', 'alive');
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ('Melissa', 2, 'male', 'grey', 'easy', 'alive');
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ('Grinch', 5, 'male', 'orange', 'easy', 'dead');
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES ('Wendy', 9, 'male', 'red', 'easy', 'dead');
+INSERT INTO bears (name, age, gender, color, temperament,alive) VALUES (NULL, 9, 'male', 'red', 'easy', 'dead');
